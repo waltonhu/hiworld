dbcdd29c16604095dc843f023654ce65c7f8b93d
waltonhu <waltonhu@163.com>
2020-08-09 17:16:53 +0800

    test

---
+++ src/HiWorld.java
@@ -0,0 +1,10 @@
+/**
+ * @program: hiworld
+ * @description:
+ * @author: Walton Hu  2020/08/09 17:13
+ */
+public class HiWorld {
+    public static void main(String[] args) {
+        System.out.println("Hi World!");
+    }
+}
